30d9c9cc7dbbb28c29c32ec1282388e54d5601b5
Chris Jones <info@csjones.co.za>
2019-04-25 14:21:12 +0000

    CRUD complete

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -12,24 +12,35 @@ class TodosController < ApplicationController
     else
       render 'new'
     end
+  end
     
-    def show
-      @todo = Todo.find(params[:id])
-    end
+  def show
+    @todo = Todo.find(params[:id])
+  end
     
-    def edit
-      @todo = Todo.find(params[:id])
-    end
+  def edit
+    @todo = Todo.find(params[:id])
+  end
     
-    def update
-      @todo = Todo.find(params[:id])
-      if @todo.update(todo_params)
-        flash[:notice] = " Todo was succesfully updated"
-        redirect_to todo_path(@todo)
-      else
-        render "edit"
-      end
+  def update
+    @todo = Todo.find(params[:id])
+    if @todo.update(todo_params)
+      flash[:notice] = " Todo was succesfully updated"
+      redirect_to todo_path(@todo)
+    else
+      render "edit"
     end
+  end
+ 
+  def index
+     @todos = Todo.all
+  end
+  
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    flash[:notice] = "Todo was deleted succesfully"
+    redirect_to todos_path
   end
   
   private
